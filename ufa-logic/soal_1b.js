56c73ff5a8a0fd8a864e0e061779693488421539
agunbuhori <agun.buhori@gmail.com>
2021-11-16 01:07:06 +0700

    unnecesary files deleted

--- ufa-logic/soal_1b.js
+++ ufa-logic/soal_1b.js
@@ -1,14 +1,14 @@
 function pattern_count(str, substr) {
  const strlen = str.length
  const substrlen = substr.length
- let result = 0;
+ let result = 0
 
  if (strlen === 0 || substrlen === 0) return result
  else if (strlen > 100) return 'Maximum 100 characters'
 
  for (let i = 0; i < strlen - substrlen + 1; i++) {
   if (str.slice(i, i+substrlen) === substr) {
-    result++;
+    result++
   }
  }
 
